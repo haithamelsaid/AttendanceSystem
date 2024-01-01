64baab8847a0c1689d014655531596109fbe4501
Haitham ELsaid <haithamelsaid9@gmail.com>
2024-01-01 23:05:46 +0200

    update

--- Task-attendanceSystem/ViewModels/LoginVM.cs
+++ Task-attendanceSystem/ViewModels/LoginVM.cs
@@ -4,9 +4,10 @@ namespace Task_attendanceSystem.ViewModels
 {
     public class LoginVM
     {
-        public string UserName { get; set; }
+        public string UserName { get; set; 
+
         [DataType(DataType.Password)]
-        public string Password { get; set; }
+        public string Password { get; set; } 
         public bool RememberMe { get; set; }
     }
 }
